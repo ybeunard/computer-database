cdaee413595428d241f1ccc15400e0da6b31d74d
Yoann BEUNARDEAU <ybeunardeau@excilys.com>
2017-02-20 11:29:53 +0100

    Création de la classe OrdinateurDao

---
+++ src/com/cdb/dao/OrdinateurDao.java
@@ -0,0 +1,57 @@
+package com.cdb.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import com.cdb.persistance.Ordinateur;
+
+public class OrdinateurDao {
+	
+	private static String URL = "jdbc:mysql://localhost:3306/computer-database-db";
+    private static String LOGIN = "root";
+    private static String PASSWORD = "";
+    private final static String QUERY_FIND_ELEVES = "SELECT * FROM computer ";
+
+	public List<Ordinateur> findOrdinateur(){
+		
+		List<Ordinateur> ordinateurs = new ArrayList<Ordinateur>();
+		
+		Connection con = null; 
+		Statement stmt = null;
+		try {
+            con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            stmt = con.createStatement();
+            ResultSet rset = stmt.executeQuery(QUERY_FIND_ELEVES);
+
+            while (rset.next()) {
+            	String name = rset.getString("name");
+                Ordinateur ordinateur = new Ordinateur(name);
+                ordinateurs.add(ordinateur);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+		return ordinateurs;
+	}
+}
