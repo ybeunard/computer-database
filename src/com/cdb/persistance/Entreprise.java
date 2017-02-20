e2199c45ac55c048b67db053c12e42eea0422d9d
Yoann BEUNARDEAU <ybeunardeau@excilys.com>
2017-02-20 11:06:40 +0100

    Création de la classe Entreprise

---
+++ src/com/cdb/persistance/Entreprise.java
@@ -0,0 +1,15 @@
+package com.cdb.persistance;
+
+public class Entreprise {
+
+	private String name;
+
+	public Entreprise(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
