ea1ead6111797b9211c6a5faa4624fedf091e337
Yoann BEUNARDEAU <ybeunardeau@excilys.com>
2017-02-21 11:51:54 +0100

    Ajout de la fonction updateOrdinateur qui traduit la commande utilisateur en modification d'un ordinateur

--- src/com/cdb/services/GestionEntryUser.java
+++ src/com/cdb/services/GestionEntryUser.java
@@ -26,11 +26,17 @@ public class GestionEntryUser {
 				System.out.println("Nombre d'argument incorecte");
 				return;
 			}
-			Ordinateur ordinateur = createOrdinateur(splitArray[1]);
-			if(ordinateur != null)OrdinateurDao.getInstanceOrdinateurDao().createOrdinateur(ordinateur);
+			Ordinateur ordinateurCreated = createOrdinateur(splitArray[1]);
+			if(ordinateurCreated != null)OrdinateurDao.getInstanceOrdinateurDao().createOrdinateur(ordinateurCreated);
 			break;
 		case "update":
-			
+			if(splitArray.length != 2){
+				System.out.println("Nombre d'argument incorecte");
+				return;
+			}
+			Ordinateur ordinateurUpdated = updateOrdinateur(splitArray[1]);
+			if(ordinateurUpdated != null)OrdinateurDao.getInstanceOrdinateurDao().createOrdinateur(ordinateurUpdated);
+			break;
 		case "delete":
 			
 		case "help":
@@ -40,15 +46,12 @@ public class GestionEntryUser {
 		}
 	}
 	
-	public static Ordinateur createOrdinateur(String args){
+	//permet de recuperer les arguments et de creer un ordinateur.
+	private static Ordinateur createOrdinateur(String args){
 		
 		String[] argArray = args.split("'", 3);
 		Ordinateur ordinateur = new Ordinateur(argArray[1]);
 		if(argArray[2].isEmpty()){
-			System.out.println("Ordinateur : " +ordinateur.getName() + "\t" + ordinateur.getDateIntroduit() + "\t" + ordinateur.getDateInterrompu());
-	        if(ordinateur.getFabricant() != null){
-	        	System.out.println(ordinateur.getFabricant().getName() + "\n");
-	        }
 			return ordinateur;
 		}
 		argArray = argArray[2].split(" ", 2);
@@ -56,13 +59,14 @@ public class GestionEntryUser {
 		while(argArray.length > 1){
 			argArray = args.split(" ", 2);
 			if(argArray.length != 2){
-				System.out.println("Nombre d'argument incorecte " + argArray[0] + argArray.length);
+				System.out.println("Nombre d'argument incorecte ");
 				return null;
 			}
 			switch(argArray[0]){
 			
 			case "introduction":
 				try {
+					argArray = argArray[1].split(" ", 2);
 					SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
 			        Date parsed = format.parse(argArray[0]);
 					ordinateur.setDateIntroduit(new java.sql.Date(parsed.getTime()));
@@ -73,6 +77,7 @@ public class GestionEntryUser {
 				break;
 			case "interruption":
 				try {
+					argArray = argArray[1].split(" ", 2);
 					SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
 			        Date parsed = format.parse(argArray[0]);
 					ordinateur.setDateInterrompu(new java.sql.Date(parsed.getTime()));
@@ -84,8 +89,8 @@ public class GestionEntryUser {
 			case "fabricant":
 				try{
 					argArray = argArray[1].split(" ", 2);
-					int i = Integer.parseInt(argArray[0]);
-					Entreprise fabricant = EntrepriseDao.getInstanceEntrepriseDao().findEntrepriseByID(i);
+					int id = Integer.parseInt(argArray[0]);
+					Entreprise fabricant = EntrepriseDao.getInstanceEntrepriseDao().findEntrepriseByID(id);
 					if(fabricant != null){
 						ordinateur.setFabricant(fabricant);
 					}
@@ -103,14 +108,99 @@ public class GestionEntryUser {
 				return null;
 			}
 			if(argArray.length < 2){
-				System.out.println("Ordinateur : " +ordinateur.getName() + "\t" + ordinateur.getDateIntroduit() + "\t" + ordinateur.getDateInterrompu());
-		        if(ordinateur.getFabricant() != null){
-		        	System.out.println(ordinateur.getFabricant().getName() + "\n");
-		        }
 				return ordinateur;
 			}
 			args = argArray[1];
 		}
+		System.out.println("Nombre d'argument incorecte ");
+		return null;
+	}
+	
+	//permet de recuperer les arguments et de update un ordinateur.
+	private static Ordinateur updateOrdinateur(String args) {
+		String[] argArray = args.split(" ", 2);
+		if(argArray.length != 2){
+			System.out.println("Nombre d'argument incorecte ");
+			return null;
+		}
+		Ordinateur ordinateur;
+		try{
+			int id = Integer.parseInt(argArray[0]);
+			ordinateur = OrdinateurDao.getInstanceOrdinateurDao().findOrdinateurByID(id);
+			if(ordinateur == null){
+				System.out.println("Veuillez donner un id d'ordinateur correct");
+				return null;
+			}
+		}catch(NumberFormatException e){
+			System.out.println("Veuillez donner un id d'ordinateur correct");
+			return null;
+		}
+		args = argArray[1];
+		while(argArray.length > 1){
+			argArray = args.split(" ", 2);
+			if(argArray.length != 2){
+				System.out.println("Nombre d'argument incorecte " + argArray.length);
+				return null;
+			}
+			switch(argArray[0]){
+			
+			case "name":
+				argArray = argArray[1].split(" ", 2);
+				argArray = argArray[0].split("'", 3);
+				ordinateur.setName(argArray[1]);
+				if(argArray[2].isEmpty()){
+					return ordinateur;
+				}
+				break;
+			case "introduction":
+				try {
+					argArray = argArray[1].split(" ", 2);
+					SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+			        Date parsed = format.parse(argArray[0]);
+					ordinateur.setDateIntroduit(new java.sql.Date(parsed.getTime()));
+				} catch (ParseException e) {
+					System.out.println("Format de date incorecte marci de respecter la syntaxe suivante : yyyy/MM/dd");
+					return null;
+				}
+				break;
+			case "interruption":
+				try {
+					argArray = argArray[1].split(" ", 2);
+					SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+			        Date parsed = format.parse(argArray[0]);
+					ordinateur.setDateInterrompu(new java.sql.Date(parsed.getTime()));
+				} catch (ParseException e) {
+					System.out.println("Format de date incorecte marci de respecter la syntaxe suivante : yyyy/MM/dd");
+					return null;
+				}
+				break;
+			case "fabricant":
+				try{
+					argArray = argArray[1].split(" ", 2);
+					int id = Integer.parseInt(argArray[0]);
+					Entreprise fabricant = EntrepriseDao.getInstanceEntrepriseDao().findEntrepriseByID(id);
+					if(fabricant != null){
+						ordinateur.setFabricant(fabricant);
+					}
+					else{
+						System.out.println("Veuillez donner un id d'entreprise correct");
+						return null;
+					}
+				}catch(NumberFormatException e){
+					System.out.println("Veuillez donner un id d'entreprise correct");
+					return null;
+				}
+				break;
+			default:
+				System.out.println("Argument : " + argArray[1] + " inconnue");
+				return null;
+			}
+			if(argArray.length < 2){
+				return ordinateur;
+			}
+			args = argArray[1];
+		}
+		System.out.println("Nombre d'argument incorecte ");
 		return null;
 	}
 }
--- src/com/cdb/test/TestGestionEntryUser.java
+++ src/com/cdb/test/TestGestionEntryUser.java
@@ -9,5 +9,10 @@ public class TestGestionEntryUser {
 		GestionEntryUser.lectureEntryUser("create 'newOrdinateur'");
 		GestionEntryUser.lectureEntryUser("create 'newOrdinateur' fabricant 3");
 		GestionEntryUser.lectureEntryUser("create 'newOrdinateur' fabricant tj");
+		
+		GestionEntryUser.lectureEntryUser("update 75 name 'newOrdinateur'");
+		GestionEntryUser.lectureEntryUser("update 76 introduction 2012/12/12 name 'newOrdinateur' fabricant 9");
+		GestionEntryUser.lectureEntryUser("update 77 introduction 2022/12/12 fabricant 9");
+		GestionEntryUser.lectureEntryUser("update 79 introduction 2002/12/12 interruption 2002/12/15");
 	}
 }
