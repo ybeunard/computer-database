08f2df2c6f16f67f8ab71633db9d2ef3b2e52556
Yoann BEUNARDEAU <ybeunardeau@excilys.com>
2017-03-03 11:10:59 +0100

    renomage du fichier my.jquery.js en addComputer.js et changement des selecteurs js en selecteur jquery

--- src/main/webapp/js/addComputer.js
+++ src/main/webapp/js/addComputer.js
@@ -1,20 +1,20 @@
 $(function() {
 $("#discontinued").change(function() {
-   var startDate = document.getElementById("introduced").value;
-   var endDate = document.getElementById("discontinued").value;
+   var startDate = $("#introduced").val();
+   var endDate = $("#discontinued").val();
 
    if ((Date.parse(endDate) <= Date.parse(startDate))) {
      alert("La date d interruption doit etre plus grande que la date d introduction");
-     document.getElementById("discontinued").value = "";
+     $("#discontinued").val("");
    }
 });
 $("#introduced").change(function() {
-   var startDate = document.getElementById("introduced").value;
-   var endDate = document.getElementById("discontinued").value;
+   var startDate = $("#introduced").val();
+   var endDate = $("#discontinued").val();
 
    if ((Date.parse(endDate) <= Date.parse(startDate))) {
      alert("La date d introduction doit etre plus petite que la date d interruption");
-     document.getElementById("discontinued").value = "";
+     $("#introduced").val("");
    }
 });
  $("form[name='newComputer']").validate({
